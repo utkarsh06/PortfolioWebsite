716ce45d92b3d498d5344fe7425aaf70af340513
utkarsh06 <utkarsh.gaur06@gmail.com>
2021-01-03 20:52:10 -0500

    Added about,contact and navigation.html

--- UtkarshGaurWebsite/views.py
+++ UtkarshGaurWebsite/views.py
@@ -1,4 +1,10 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Homepage")
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
